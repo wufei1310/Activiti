f879c7d22f0e499053af192262aa228ed337f4e7
jimmy <jimmyclasstl@gmail.com>
2018-05-20 14:56:09 +0800

    spring boot2

--- modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/actuate/endpoint/ProcessEngineEndpoint.java
+++ modules/activiti-spring-boot/spring-boot-starters/activiti-spring-boot-starter-basic/src/main/java/org/activiti/spring/boot/actuate/endpoint/ProcessEngineEndpoint.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,10 +17,9 @@ import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.activiti.engine.impl.persistence.deploy.DefaultDeploymentCache;
 import org.activiti.engine.impl.persistence.deploy.DeploymentCache;
 import org.activiti.engine.impl.persistence.deploy.ProcessDefinitionCacheEntry;
-import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.repository.ProcessDefinition;
-import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 
 import java.util.*;
 
@@ -30,18 +29,18 @@ import java.util.*;
  *
  * @author Josh Long
  */
-@ConfigurationProperties(prefix = "endpoints.activiti")
-public class ProcessEngineEndpoint extends AbstractEndpoint<Map<String, Object>> {
+//@ConfigurationProperties(prefix = "endpoints.activiti")
+@Endpoint(id = "activiti")
+public class ProcessEngineEndpoint {
 
     private final ProcessEngine processEngine;
 
     public ProcessEngineEndpoint(ProcessEngine processEngine) {
-        super("activiti");
         this.processEngine = processEngine;
     }
 
-    @Override
-    public Map<String, Object> invoke() {
+    @ReadOperation
+    public Map<String, Object> activiti() {
 
         Map<String, Object> metrics = new HashMap<String, Object>();
 
